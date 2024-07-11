eb88871e3b3627398a691d5b9298a9c1fcd3d226
amitdutta <duttaamit356@gmail.com>
2024-07-11 18:12:29 +0530

    api setup

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '590fff2ce5mshc8f5726261bc514p10345bjsn2e1205f0f252',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
